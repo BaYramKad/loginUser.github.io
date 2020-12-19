db909ed5bc4cf8a12604ad0dc0e6ae92a641de4f
Bayram <bayram.kad@gmail.com>
2020-12-19 21:33:07 +0300

    Авторизация и регистрация пользователя, осталось доделать Авторизацию)

--- script.js
+++ script.js
@@ -1,42 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
     "use strict";
-    const output = document.getElementById("output");
-
-    const getRequest = (url) => {
-        return new Promise( function(resolve, reject){
-            const request = new XMLHttpRequest();
-            request.open("GET", url);
-            request.addEventListener("readystatechange", () => {
-                
-                if (request.readyState !== 4) {
-                    return;
-                }
-
-                if (request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                    resolve(data);
-                } else {
-                    reject(request.statusText);
-                }
-
-            });
-            request.send();
-        });
-    };
-
-    const getData = (data) => {
-        console.log(data);
-        data.forEach( item => {
-            output.insertAdjacentHTML("beforebegin", `<h5>${item.title}<h5>
-                                    <img src="${item.thumbnailUrl}">`);
-        });
-    };
-    const oneImg = getRequest("https://jsonplaceholder.typicode.com/photos/1"),
-        twoImg = getRequest("https://jsonplaceholder.typicode.com/photos/2");
-
+    const buttons = document.querySelector(".buttons"),
+        output = document.querySelector(".output"),
+        strong = document.getElementById("strong"),
+        section = document.querySelector(".section");
     
-    Promise.all([oneImg, twoImg])
-        .then(getData)
-        .catch( (error) => console.log(error));
+    buttons.addEventListener("click", event => {
+        let target = event.target;
+        if (target.matches("#registration")) {
+            registrationUser();
+        } else if (target.matches("#login")) {
+            loginUse();
+        }
+    });
 
+    const userData = function(){
+        return JSON.parse(localStorage.getItem("userData")) || localStorage.setItem("userData", JSON.stringify([]));
+    }
+    userData();
+    
+    const getTime = () => {
+        let dateUserReg = new Date(),
+            time = dateUserReg.toLocaleTimeString("ru"),
+            date = dateUserReg.toLocaleDateString("ru");
+        const dateNow = [date, time].join("г., ");
+        return dateNow;
+    };
+
+    const registrationUser = () => {
+        const time = getTime();
+        let  user = prompt("Введите через пробел Имя и Фамилию пользователя").split(" "),
+            login = prompt("Введите Логин"),
+            password = prompt("Введите Пароль");
+
+        while (user, login, password === ""){
+            user = prompt("Вы не указали Имя, логин или пароль пользователя").split(" "),
+            login = prompt("Логин"),
+            password = prompt("Пароль");
+        }
+        let data = userData();
+        data.push({
+            login,
+            password,
+            time,
+            name : user[0],
+            lastName : user[1]
+        }); 
+        localStorage.setItem("userData", JSON.stringify(data));
+        render();
+    };
+
+    const render = () => {
+        output.innerHTML = "";
+        const dataUserStorage = JSON.parse(localStorage.getItem("userData"));
+        dataUserStorage.forEach((item, i) => {
+            if (item.lastName === undefined || item.lastName === "") {
+                item.lastName = "Не указана";
+            }
+            let newUser = `<li data-id=${i}>Имя: ${item.name}, Фамилия: ${item.lastName}, Зарегестрирован: ${item.time} <button data-id="${i}" id="btn-close">Удалить</button></li> `;
+            output.insertAdjacentHTML("beforeend", newUser);
+        });
+        
+    };
+    render();
+
+    const closeLi = (elem) => {
+        let data = userData();
+        data.splice(elem, 1);
+        localStorage.setItem("userData", JSON.stringify(data))
+        render();
+    };
+    section.addEventListener("click", event => {
+        let target = event.target;
+        if (target.matches("#btn-close")) {
+            closeLi(+target.closest("li").dataset.id)
+        }
+    });
+
+    const loginUse = () => {
+        const dataUser = prompt("Введите логни"),
+            password = prompt("Введите пароль");
+    };
 });
+    
